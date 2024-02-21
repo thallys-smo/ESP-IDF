fe17de9809cafa4b4b2f937e73a0f57ab79cda37
Thallys Oliveira <thallys.smo@gmail.com>
2024-02-21 17:43:33 -0300

    Função Dado Aleatório Implementada

--- 3_Numero_Aleatorio/main/main.c
+++ 3_Numero_Aleatorio/main/main.c
@@ -5,8 +5,19 @@
 #include "esp_random.h"
 #include "esp_system.h"
 #include <intr_types.h>
+#include "math.h"
 
 #define TAG "DADO"
+
+// Função que simula um Dado aleatório
+int dado(){
+    int random = esp_random();
+    int num_positivo = abs(random);
+    int num_dado = (num_positivo % 6) +1;
+
+    return num_dado; 
+
+}
 
 void app_main(void)
 {
@@ -14,7 +25,10 @@ void app_main(void)
     while(1){
         int i = esp_random();
         vTaskDelay(1000/portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "numero aleatorio: %d", i);
+        // Implementação sem correção do número aleatório
+        ESP_LOGI(TAG, "Numero Aleatório: %d", i);
+        // Implementação da função dado, utilizando a a função aleatória
+        ESP_LOGE(TAG, "Dado Aleatório: %d", dado());
     }
     
 }
