c94b7786b3e13c8bad33fae8e464d8de0f305857
Thallys Oliveira <thallys.smo@gmail.com>
2024-02-22 09:28:52 -0300

    Trabalhando com Headers

---
+++ 3. Componetes e Bibliotecas/1_Referenciando_C/main/calculadora/calculadora.h
@@ -0,0 +1,6 @@
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
+
+int soma (int a, int b);
+
+#endif
---
+++ 3. Componetes e Bibliotecas/1_Referenciando_C/main/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "calculadora/calculadora.h"
+
+void app_main(void)
+{
+    int result = soma (3,5);
+    printf("Resultado: %d\n", result);
+}
+
